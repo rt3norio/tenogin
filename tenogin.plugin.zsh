21cd962c08b6f061045c7ad342d399014ffde87c
Rodrigo Tenorio <rtenorio@funcionalcorp.com.br>
2021-02-24 11:14:52 -0300

    reorganizando o arquivo

--- tenogin.plugin.zsh
+++ tenogin.plugin.zsh
@@ -1,22 +1,26 @@
-alias ytmp3="youtube-dl --extract-audio --audio-format mp3"
-alias dcd="docker-compose down --remove-orphans"
-alias dcu="docker-compose up -d"
-alias dpbm="docker exec -it php-pbm bash"
-alias dtt="docker exec -it php-tradetools bash"
-alias dcdu="dcd && dcu"
+###queridinhos###
+
 alias redisflush="docker exec -it redis redis-cli flushall"
 alias pgsql="docker exec -it postgres psql -U webadm"
-alias ttworker="docker exec -i php-tradetools ./protected/yiic worker umTrabalho"
-alias ttyiic="docker exec -i php-tradetools ./protected/yiic"
-alias aliases="vim ~/linux-config/.zsh_aliases"
-alias tt="cd ~/projetos/tradetools"
-alias i3conf="vim ~/.config/i3/config"
+alias aliases="vim ~/.oh-my-zsh/plugins/tenogin/tenogin.plugin.zsh"
 alias mata-tudo='echo "--- parando as imagens Docker em execução ---";docker kill $(docker ps -q);docker rm $(docker ps -a -q)'
-alias wireless-config="nmtui"
 alias mysqli='docker exec -it mysql mysql -U webadm --password="A123456"'
-alias testesTradetools='tt && ./protected/data/recria_bases_paralelo.sh rt -vv'
-alias umTesteTradetools="docker exec -it php-tradetools ./vendor/bin/phpunit "
 alias tudomeu="sudo chown $UID:$GID * -R"
+
+dips() {
+  docker ps -q | xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }}' | sed 's/ \// /' | grep "$1"
+}
+
+dce(){
+  docker-compose exec php bash
+}
+
+deb() {
+  echo "running: docker exec -it "$1" bash\n"
+  docker exec -it "$1" bash
+}
+
+
 ##Estatisticas dos comandos mais utilizados
 hstatistics(){
   echo "Estatistica para dois comandos"
@@ -25,10 +29,32 @@ hstatistics(){
   history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20;
 }
 
-
 ##CD + pasta projetos com autocomplete
 cdp() { cd ~/projetos/$1; }
 compctl -W ~/projetos/ -/ cdp
+
+
+
+
+
+alias ytmp3="youtube-dl --extract-audio --audio-format mp3"
+alias dcd="docker-compose down --remove-orphans"
+alias dcu="docker-compose up -d"
+alias dpbm="docker exec -it php-pbm bash"
+alias dtt="docker exec -it php-tradetools bash"
+alias dcdu="dcd && dcu"
+alias ttworker="docker exec -i php-tradetools ./protected/yiic worker umTrabalho"
+alias ttyiic="docker exec -i php-tradetools ./protected/yiic"
+alias tt="cd ~/projetos/tradetools"
+alias i3conf="vim ~/.config/i3/config"
+alias wireless-config="nmtui"
+alias testesTradetools='tt && ./protected/data/recria_bases_paralelo.sh rt -vv'
+alias umTesteTradetools="docker exec -it php-tradetools ./vendor/bin/phpunit "
+
+
+
+
+
 
 horizon(){
   docker-compose exec php php artisan horizon
@@ -42,19 +68,6 @@ gitlogtudo(){
 git diff --stat origin/master..origin/"$(git branch | grep \* | cut -d ' ' -f2)" --no-merges
 }
 
-deb() {
-  echo "running: docker exec -it "$1" bash\n"
-  docker exec -it "$1" bash
-}
-
-dce(){
-  docker-compose exec php bash
-}
-
-
-dips() {
-  docker ps -q | xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }}' | sed 's/ \// /' | grep "$1"
-}
 
 deleteBranch(){
 	git branch -D $1
